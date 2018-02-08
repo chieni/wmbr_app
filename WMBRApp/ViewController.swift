e42f8150912028b384c6d7ace0c670ba76ee7534
chieni <mitchieni@gmail.com>
2018-02-08 15:43:29 -0500

    first

--- WMBRApp/ViewController.swift
+++ WMBRApp/ViewController.swift
@@ -7,9 +7,29 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player = AVPlayer()
+    @IBAction func playVideo(_ sender: Any) {
+        guard let url = URL(string:  "http://wmbr.org/WMBR_live_128.m3u") else {
+            return
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+        
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
